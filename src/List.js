3a01699f254c32310d0123c61ca032ae509cf5d3
SNicholson99 <scott.nicholson@live.co.uk>
2017-11-03 13:30:16 +0000

    created to do list

---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React  from 'react';
+
+const List =(props) => (
+    <ul>
+      {props.items.map((items, index)=><li key={index}>{items}</li>)}
+      </ul>
+);
+
+
+
+
+export default List;
