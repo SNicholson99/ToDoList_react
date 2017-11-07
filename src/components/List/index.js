096ea660ce6dc1a551f3e5190d19ee01d8b1e4d7
SNicholson99 <scott.nicholson@live.co.uk>
2017-11-07 10:06:55 +0000

    added styling

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,14 +1,34 @@
 import React  from 'react';
 
-const List =(props) => (
-  <ol>
-    {props.items.map((items, index)=>
-      <li key={index}>{items.toDo}
-        <button onClick={()=>props.onComplete(index)}>{items.complete ? "Complete" : "Incomplete"}</button>
-      </li>)}
-  </ol>
-);
+const staticStyle = {
+  padding: "10px",
+  fontStyle : "italic",
+  fontWeight : "bold",
+  margin : "10px",
+  borderRadius : "10px",
+}
 
+const List =(props) => {
+  const style = {
+    complete : {
+      backgroundColor : "rgb(0, 255, 163)",
+      borderColor: "rgb(0, 255, 163)"
+    },
+    incomplete : {
+      backgroundColor : "rgb(255, 0, 0)",
+      borderColor: "rgb(255, 0, 0)",
+      color: "white",
+    }
+  };
+  return (
+    <ol>
+      {props.items.map((items, index)=>
+        <li key={index}>{items.toDo}
+          <button style={{...staticStyle,...style[items.complete ? "complete" : "incomplete"]}} onClick={()=>props.onComplete(index)}>{items.complete ? "Complete" : "Incomplete"}</button>
+        </li>)}
+    </ol>
+  );
+}
 
 
 
